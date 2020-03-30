7bf5aecfbf9652399e3cf32b5e1984462b82af13
CharukaHS <charuka@protonmail.com>
2020-03-30 12:49:04 +0530

    Fetch posts from firestore and save in store

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,34 @@
 <script>
+  import { onMount, onDestroy } from "svelte";
+  import { postsArr } from "../store/";
+
   import Post from "../components/grid/Post.svelte";
+
+  const db = firebase.firestore();
+  const ppq = 9; // post-per-query
+  let posts;
+  $: posts;
+
+  const unsubscribe = postsArr.subscribe(value => {
+    posts = value;
+    console.log(posts);
+
+    if (posts.length == 0) {
+      db.collection("posts")
+        .orderBy("timestamp", "desc")
+        .limit(ppq)
+        .get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            posts = [...posts, doc.data()]
+          });
+          postsArr.set(posts);
+        })
+        .catch(e => console.error(e));
+    }
+  });
+
+  onDestroy(unsubscribe);
 </script>
 
 <svelte:head>
@@ -10,16 +39,7 @@
   id="postContainer"
   class="flex flex-wrap content-start justify-center box-border h-auto bg-gray
   px-8">
-  <Post />
-  <Post />
-  <Post />
-  <Post />
-  <Post />
-  <Post />
-  <Post />
-  <Post />
-  <Post />
-  <Post />
-  <Post />
-  <Post />
+  {#each posts as post}
+    <Post />
+  {/each}
 </div>
