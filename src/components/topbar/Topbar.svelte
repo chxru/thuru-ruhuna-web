1fbf7372f5ebf45252296dec64e6201586255cfe
CharukaHS <charuka@protonmail.com>
2020-04-08 03:23:29 +0530

    Relocate signout button in a dropdown menu

--- src/components/topbar/Topbar.svelte
+++ src/components/topbar/Topbar.svelte
@@ -5,6 +5,7 @@
 
   let name;
   let isLogged;
+  let isDropdown;
 
   const unsubscribe = username.subscribe(value => {
     name = value;
@@ -21,16 +22,23 @@
   };
 
   const signout = () => {
-    firebase.auth().signOut().then(() => {
-      userDetails.set({})
-    }).catch(e => {
-      console.error(e)
-    })
-  }
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        userDetails.set({});
+      })
+      .catch(e => {
+        console.error(e);
+      });
+  };
+
+  const dropdownClicked = () => {
+    isDropdown = !isDropdown;
+  };
 </script>
 
-<div
-  class="flex justify-between h-24 w-full px-8 py-8 md:pl-12 bg-gray">
+<div class="flex justify-between h-24 w-full px-8 py-8 md:pl-12 bg-gray">
   <div class="flex flex-no-wrap font-sans text-xl text-black cursor-pointer">
     <img src="/leaves.svg" class="h-8 mr-4" alt="Thuru Ruhuna Logo" />
     <span class="w-full" on:click={navigateHome}>Thuru Ruhuna</span>
@@ -43,10 +51,24 @@
         src="/defaultuser.webp"
         class="h-8 ml-4 rounded-full hidden md:inline object-cover"
         alt="user's image" />
-    {/if}
-    <img src="/down-arrow.svg" class="h-2 mt-2 ml-4" alt="down arrow" />
-    {#if isLogged}
-      <img src="/signout.svg" class="h-4 mt-2 ml-4" alt="signout" on:click={signout} />
+      <img
+        src="/down-arrow.svg"
+        class="h-2 mt-2 ml-4"
+        alt="down arrow"
+        on:click={dropdownClicked} />
     {/if}
   </div>
 </div>
+
+{#if isDropdown}
+  <span
+    class="fixed text-right h-auto w-32 bg-gray shadow-lg rounded-lg z-20 p-4"
+    style="top: 70px; right: 20px;">
+    <div
+      class="flex flex-no-wrap justify-center cursor-pointer"
+      on:click={signout}>
+      Signout
+      <img src="/signout.svg" class="h-4 mt-2 ml-4" alt="signout" />
+    </div>
+  </span>
+{/if}
