f90f3c2460d1c6f0d06dfcf1e19cc994ca514d9c
CharukaHS <charuka@protonmail.com>
2020-06-18 14:25:47 +0530

    Add a loading screen till external js load

---
+++ src/components/splashscreen/Splash.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col justify-center items-center h-screen">
+  <img src="/leaves.svg" alt="Icon" class="h-32 mb-4" />
+  <h1>Loading</h1>
+</div>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,12 +2,24 @@
   import { onMount, onDestroy } from "svelte";
   import { userDetails, isLoggedIn } from "../store";
 
-  import Topbar from "../components/topbar/Topbar.svelte";
-  import Sidebar from "../components/sidebar/Sidebar.svelte";
   import Alert from "../components/alert/Alert.svelte";
+  import Sidebar from "../components/sidebar/Sidebar.svelte";
+  import Splash from "../components/splashscreen/Splash.svelte";
+  import Topbar from "../components/topbar/Topbar.svelte";
 
   let isLogged;
+  let doneLoading = false;
   export let segment;
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBIlVA3dYbid9YvNytPllMirXO8GA1EZcQ",
+    authDomain: "thuru-ruhuna-dev.firebaseapp.com",
+    databaseURL: "https://thuru-ruhuna-dev.firebaseio.com",
+    projectId: "thuru-ruhuna-dev",
+    storageBucket: "thuru-ruhuna-dev.appspot.com",
+    messagingSenderId: "47791534606",
+    appId: "1:47791534606:web:60286c1455f2fd1334025f"
+  };
 
   const unsubscribe = isLoggedIn.subscribe(value => {
     isLogged = value;
@@ -15,6 +27,8 @@
 
   // check the user has signed in before
   onMount(() => {
+    firebase.initializeApp(firebaseConfig);
+    doneLoading = true;
     const auth = firebase.auth();
     const db = firebase.firestore();
 
@@ -33,13 +47,17 @@
   onDestroy(unsubscribe);
 </script>
 
-{#if !segment || segment == "profile" || "auth"}
+{#if !segment || segment == 'profile' || 'auth'}
   <div class="h-screen bg-gray">
-    <Topbar />
-    <Sidebar />
-    <div class="h-screen pt-4 pl-0 md:pl-8">
-      <slot />
-    </div>
+    {#if doneLoading}
+      <Topbar />
+      <Sidebar />
+      <div class="h-screen pt-4 pl-0 md:pl-8">
+        <slot />
+      </div>
+    {:else}
+      <Splash />
+    {/if}
     <Alert />
   </div>
 {/if}
